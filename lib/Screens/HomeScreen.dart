32c7a80bfacec369a7e165eadd9b9fbab864198d
Barnabas <barnabasgavai@gmail.com>
2024-02-03 21:45:44 +0530

    Working well 2

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -36,8 +36,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   color: Colors.black,
                 )),
           ],
-          systemOverlayStyle:
-              const SystemUiOverlayStyle(statusBarColor: Colors.white),
         ),
         body: BlocBuilder<UserBloc, UserState>(
           builder: (context, state) {
@@ -59,143 +57,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 itemCount: data.length,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.fromLTRB(11, 8, 11, 8),
-                    child: Container(
-                      height: 100,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(255, 192, 192, 192),
-                            offset: Offset(0, 0),
-                            blurRadius: 3,
-                            spreadRadius: 1.2,
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              SvgPicture.string(
-                                """<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	 width="800px" height="800px" viewBox="796 796 200 200" enable-background="new 796 796 200 200" xml:space="preserve">
-<path d="M896,796c-55.14,0-99.999,44.86-99.999,100c0,55.141,44.859,100,99.999,100c55.141,0,99.999-44.859,99.999-100
-	C995.999,840.86,951.141,796,896,796z M896.639,827.425c20.538,0,37.189,19.66,37.189,43.921c0,24.257-16.651,43.924-37.189,43.924
-	s-37.187-19.667-37.187-43.924C859.452,847.085,876.101,827.425,896.639,827.425z M896,983.86
-	c-24.692,0-47.038-10.239-63.016-26.695c-2.266-2.335-2.984-5.775-1.84-8.82c5.47-14.556,15.718-26.762,28.817-34.761
-	c2.828-1.728,6.449-1.393,8.91,0.828c7.706,6.958,17.316,11.114,27.767,11.114c10.249,0,19.69-4.001,27.318-10.719
-	c2.488-2.191,6.128-2.479,8.932-0.711c12.697,8.004,22.618,20.005,27.967,34.253c1.144,3.047,0.425,6.482-1.842,8.817
-	C943.037,973.621,920.691,983.86,896,983.86z"/>
-</svg>""",
-                                width: 70,
-                                height: 70,
-                                color: Colors.grey,
-                              ),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        data[index].name,
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      const SizedBox(
-                                        width: 20,
-                                      ),
-                                      data[index].gender == "male"
-                                          ? const Row(
-                                              children: [
-                                                Icon(
-                                                  Icons.male,
-                                                  size: 23,
-                                                ),
-                                                SizedBox(
-                                                  width: 3,
-                                                ),
-                                                Text(
-                                                  "Male",
-                                                  style:
-                                                      TextStyle(fontSize: 15),
-                                                )
-                                              ],
-                                            )
-                                          : const Row(
-                                              children: [
-                                                Icon(
-                                                  Icons.female,
-                                                  size: 23,
-                                                ),
-                                                SizedBox(
-                                                  width: 3,
-                                                ),
-                                                Text(
-                                                  "Female",
-                                                  style:
-                                                      TextStyle(fontSize: 15),
-                                                )
-                                              ],
-                                            ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.mail_outline,
-                                        color:
-                                            Color.fromARGB(255, 131, 130, 130),
-                                        size: 23,
-                                      ),
-                                      const SizedBox(
-                                        width: 3,
-                                      ),
-                                      Text(
-                                        data[index].email,
-                                        softWrap: true,
-                                        style: const TextStyle(
-                                            fontSize: 15,
-                                            overflow: TextOverflow.ellipsis),
-                                      )
-                                    ],
-                                  ),
-                                  Container(
-                                    alignment: Alignment.centerRight,
-                                    child: const Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Icon(
-                                          Icons.edit,
-                                          size: 22,
-                                          color:
-                                              Color.fromARGB(255, 77, 86, 255),
-                                        ),
-                                        SizedBox(
-                                          width: 12,
-                                        ),
-                                        Icon(
-                                          Icons.delete_outline,
-                                          size: 22,
-                                          color: Colors.red,
-                                        )
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          )),
-                    ),
-                  );
+                      padding: const EdgeInsets.fromLTRB(11, 8, 11, 8),
+                      child: ListTile(
+                        title: Text(data[index].name),
+                        subtitle: Text(data[index].email),
+                        trailing: Text(data[index].gender),
+                      ));
                 },
               ),
             );
